30b92d3b3bf8b8c512ed98a99636b77391c74609
nahidchy <nicnahidchowdhury@gmail.com>
2022-08-26 11:03:42 +0600

    first commit

---
+++ main.js
@@ -0,0 +1,8 @@
+function yes(){
+    const text = document.getElementById('msg');
+    text.innerText= "I love you so much. You are only mine."
+}
+function no() {
+    const text = document.getElementById('msg');
+    text.innerText= "You should click left button!"
+}
